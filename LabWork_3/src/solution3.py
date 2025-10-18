7751703605766190a4d10770d68eef78574610d0
Figeiron <51824547+figeiron@users.noreply.github.com>
2025-10-18 18:03:35 +0300

    LabWork3! simplifying

--- LabWork_3/src/solution3.py
+++ LabWork_3/src/solution3.py
@@ -1,5 +1,5 @@
-def str_to_list(num):
-    return list(map(lambda x: int(x), list(num)))
+def str_to_digit_list(num):
+    return list(map(int, list(num)))
 
 
 while True:
@@ -7,15 +7,15 @@ while True:
         A = input("Введіть A: ")
         B = input("Введіть B: ")
 
-        a_base = max(str_to_list(A)) + 1
-        b_base = max(str_to_list(B)) + 1
+        a_base = max(str_to_digit_list(A)) + 1
+        b_base = max(str_to_digit_list(B)) + 1
         break
 
     except ValueError:
         print("Спробуйте знову")
 
-print(a_base)
-print(b_base)
+print("Мінімально можлива числення для числа А:", a_base)
+print("Мінімально можлива числення для числа В:", b_base)
 
-print(int(A, a_base))
-print(int(B, b_base))
+print("Число А в десятковій системі:", int(A, a_base))
+print("Число В в десятковій системі:", int(B, b_base))
